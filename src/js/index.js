bd79196026a338c7f83466e0edac58f6d4ae23e6
Jonathan Berlemont <berlemontjonathan@gmail.com>
2019-02-21 21:05:37 +0100

    Ajout 3 liens dans la page home

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,42 @@
 let vids = document.getElementsByTagName("video");
 let indicators = document.getElementsByClassName("carousel-indicators")[0];
 
+let carousel_inner = document.getElementsByClassName("carousel-inner")[0];
+
+let div_services = document.getElementById("div-services");
+let div_about = document.getElementById("div-about");
+let div_contact = document.getElementById("div-contact");
+let section_links = document.getElementById("section-links");
+
+
 window.addEventListener("load", () => {
     vids[0].play;
     indicators.style.display = "none";
+
     setTimeout(() => {
         indicators.style.display = "";
     }, 5000);
+
+
+    setTimeout(() => {
+        section_links.style.overflow = "hidden";
+        div_services.classList.add("slideFromRight");
+        div_services.classList.remove("waiting");
+    }, 5000)
+    setTimeout(() => {
+        div_about.classList.add("slideFromRight");
+        div_about.classList.remove("waiting");
+    }, 6000)
+    setTimeout(() => {
+        div_contact.classList.add("slideFromRight");
+        div_contact.classList.remove("waiting");
+    }, 7000)
+
+    setTimeout(() => {
+        section_links.style.overflow = "";
+    }, 10000);
+
+
 })
 
 let check = () => {
